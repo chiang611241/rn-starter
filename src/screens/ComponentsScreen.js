f6e34262b796d26fdd17fb564d06a62bac0283f6
JennyChiang <chihyueh871125@gmail.com>
2021-11-24 14:43:51 +0800

    Feature: add variable

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyle}>This is components screen</Text>
+  const name = "Stephen";
+  return (
+    <View>
+      <Text style={styles.textStyle}>Getting started with react native</Text>
+      <Text style={styles.subHeaderStyle}>My name is {name}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30,
-    }
+  textStyle: {
+    fontSize: 45,
+  },
+  subHeaderStyle: {
+    fontSize: 20,
+  },
 });
 
 export default ComponentsScreen;
