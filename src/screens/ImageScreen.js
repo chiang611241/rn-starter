595222e84095d815b4af93ddca10122e1a4d1da6
JennyChiang <chihyueh871125@gmail.com>
2021-12-01 17:09:27 +0800

    Exercise: show score prop

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -8,6 +8,7 @@ const ImageScreen = () => {
       <ImageDetail
         title="Forest"
         imageSource={require("../../assets/images/forest.jpg")}
+        score={9}
       />
     </View>
   );
