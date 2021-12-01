c471e390069684ba9de37767b2f1531fa400142a
JennyChiang <chihyueh871125@gmail.com>
2021-12-01 16:31:18 +0800

    Feature: add image screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,6 +13,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go To List Demo"
         onPress={() => navigation.navigate('List')}
       />
+      <Button
+        title="Go To Image Demo"
+        onPress={() => navigation.navigate('Image')}
+      />
     </View>
   );
 };
---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const ImageScreen = () => {
+    return (
+        <View></View>
+    );
+};
+
+export default ImageScreen;
