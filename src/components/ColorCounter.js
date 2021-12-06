b2ab072f4184d243683b1ac8b34847ad62e7d4e1
JennyChiang <chihyueh871125@gmail.com>
2021-12-06 10:01:06 +0800

    Exercise: study reducer(counter)

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -11,14 +11,6 @@ const ColorCounter = ({ color, onIncrease, onDecrease }) => {
   );
 };
 
-const randomRgb = () => {
-  const red = Math.floor(Math.random() * 256);
-  const green = Math.floor(Math.random() * 256);
-  const blue = Math.floor(Math.random() * 256);
-
-  return `rgb(${red}, ${green}, ${blue})`;
-};
-
 const styles = StyleSheet.create({
   container: {
     padding: 10,
--- src/screens/CountScreen.js
+++ src/screens/CountScreen.js
@@ -1,13 +1,37 @@
-import React, { useState } from "react";
-import { Button, Text, View, StyleSheet } from "react-native";
+import React, { useReducer } from 'react';
+import { Button, Text, View, StyleSheet } from 'react-native';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {
+        ...state,
+        count: state.count + action.payload,
+      };
+    case 'decrement':
+      return {
+        ...state,
+        count: state.count - action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 const CountScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const { count } = state;
   return (
     <View>
-      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
-      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
-      <Text>Count: {counter}</Text>
+      <Button
+        title='Increase'
+        onPress={() => dispatch({ type: 'increment', payload: 1 })}
+      />
+      <Button
+        title='Decrease'
+        onPress={() => dispatch({ type: 'decrement', payload: 1 })}
+      />
+      <Text>Count: {count}</Text>
     </View>
   );
 };
